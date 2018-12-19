30e65c0ac2ab983df255c3af7e055e12ad06d023
mouzmike <nmouzourakis@tickethour.com>
2018-12-19 14:02:02 +0200

    refactorting

--- src/main/java/eu/ubitech/openvas/command/AuthenticatedBuilder.java
+++ src/main/java/eu/ubitech/openvas/command/AuthenticatedBuilder.java
@@ -1,6 +1,14 @@
 package eu.ubitech.openvas.command;
 
-public abstract class AuthenticatedBuilder<Req,Res extends Response> extends CommandSender<Req,Res> {
+public abstract class AuthenticatedBuilder<Req, Res extends Response>
+    extends CommandSender<Req, Res> {
   protected String username;
   protected String password;
+
+  public AuthenticatedBuilder<Req, Res> withAuthentication(
+      final String username, final String password) {
+    this.username = username;
+    this.password = password;
+    return this;
+  }
 }
--- src/main/java/eu/ubitech/openvas/command/AuthenticationWasNotSetException.java
+++
@@ -1,9 +0,0 @@
-package eu.ubitech.openvas.command;
-
-public class AuthenticationWasNotSetException extends RuntimeException {
-  private static final long serialVersionUID = 1L;
-
-  public AuthenticationWasNotSetException() {
-		super("Authentication credentials [username, password] have not been provided!");
-	}
-}
